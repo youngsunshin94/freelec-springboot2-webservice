8df07f56cfa5c940f50b2450b084610da276b02a
youngsunshin94 <shinysys94@naver.com>
2020-10-02 01:49:26 +0900

    테스트 코드

---
+++ src/main/java/com/mycom/book/springboot/web/dto/HelloResponseDTO.java
@@ -0,0 +1,11 @@
+package com.mycom.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDTO {
+    private final String name;
+    private final int amount;
+}
---
+++ src/test/java/com/mycom/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,19 @@
+package com.mycom.book.springboot.web.dto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트() {
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDTO dto = new HelloResponseDTO(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
